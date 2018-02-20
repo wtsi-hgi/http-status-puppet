9d5fb274b9b075a35984c5be3ebcf36b8bd3d216
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-02-20 16:37:50 +0000

    Adds Python packaging boilerplate.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+try:
+    from pypandoc import convert
+    def read_markdown(file: str) -> str:
+        return convert(file, "rst")
+except ImportError:
+    def read_markdown(file: str) -> str:
+        return open(file, "r").read()
+
+setup(
+    name="httpstatuspuppet",
+    version="1.0.0",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=open("requirements.txt", "r").readlines(),
+    url="https://github.com/wtsi-hgi/http-status-puppet.git",
+    license="MIT",
+    description="TODO",
+    long_description=read_markdown("README.md"),
+    zip_safe=True
+)
